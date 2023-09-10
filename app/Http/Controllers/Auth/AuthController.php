24750209ecc492c7c8253392e164b041506c9f5e
shehroz665 <shehrozmalik665@gmail.com>
2023-09-10 02:39:17 +0100

    logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,7 +8,7 @@ use Laravel\Sanctum\HasApiTokens;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use App\Helpers\ApiResponse;
 use Illuminate\Support\Facades\DB;
 class AuthController extends Controller
@@ -20,14 +20,22 @@ class AuthController extends Controller
             $request->validate([
                 'email' => 'required|email',
                 'password' => 'required',
+                'remember_me' => 'boolean',
             ]);   
-            $password= Hash::make($request->password);
-            $user = User::where('email', $request->email)->first();
-            if (!$user || !password_verify($request->password, $user->password)) {
-                return ApiResponse::error(false,'Invalid credentials',[],500);
+            // $user = User::where('email', $request->email)->first();
+            // if (!$user || !password_verify($request->password, $user->password)) {
+            //     return ApiResponse::error(false,'Invalid credentials',[],500);
+            // }
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return ApiResponse::error(false, 'Invalid credentials', [], 500);
             }
-            $data= [
-                'token' => $user->createToken("API TOKEN")->plainTextToken,
+            $user = Auth::user();
+            $token = $request->boolean('remember_me') ?
+            $user->createToken("API TOKEN", ['*'])->plainTextToken :
+            $user->createToken("API TOKEN")->plainTextToken;
+
+            $data = [
+                'token' => $token,
             ];
             return ApiResponse::success(true,'User Logged In Successfully',$data,200);      
         } catch (\Throwable $e) {
@@ -57,4 +65,17 @@ class AuthController extends Controller
         }
 
     }  
+    public function logout(Request $request)
+    {
+        try {
+           $user = auth()->user();
+            if (!$user) {
+                return ApiResponse::error(false, 'User not authenticated', [], 401);
+            }
+            $user->tokens()->delete();
+            return ApiResponse::success(true, 'User logged out successfully');
+        } catch (\Throwable $e) {
+            return ApiResponse::error(false, $e->getMessage(), [], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,9 @@ use App\Http\Controllers\Auth\AuthController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::post('/login',[AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
