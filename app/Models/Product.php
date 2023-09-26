cf92b3e39f1f19331fc50da25e33e83c0a55680d
shehroz665 <shehrozmalik665@gmail.com>
2023-09-26 19:02:19 +0100

    Product table migration,model

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $table = 'products';
+    protected $primaryKey = 'prod_id';
+    protected $fillable = [
+        'prod_name',
+        'prod_sup_id',
+        'prod_sup_name',
+        'prod_cat_id',
+        'prod_cat_name',
+        'added_by',
+        'modified_by',
+        'status',
+        'prod_cost',
+        'prod_selling_price',
+        'image',
+    ];
+}
---
+++ database/migrations/2023_09_26_175232_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('prod_id');
+            $table->string('prod_name');
+            $table->unsignedBigInteger('prod_sup_id');
+            $table->string('prod_sup_name');
+            $table->unsignedBigInteger('prod_cat_id');
+            $table->string('prod_cat_name');
+            $table->string('added_by');
+            $table->string('modified_by')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->decimal('prod_cost', 10, 2);
+            $table->decimal('prod_selling_price', 10, 2);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
