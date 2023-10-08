95797c4e738d0d3a11d1d69bcd1e8b3711f330e8
shehroz665 <shehrozmalik665@gmail.com>
2023-10-08 05:23:16 +0500

    timezone changed from USD to Karachi/Pakistan

--- config/database.php
+++ config/database.php
@@ -89,6 +89,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'timezone' => 'Asia/Karachi'
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
